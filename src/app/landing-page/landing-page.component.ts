d9763e8a6dc9a279d8a537115a73335c1b762124
Julian Alexander <191233@ids.upchiapas.edu.mx>
2020-09-24 23:35:25 -0500

    add component name Landing page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -5,11 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
