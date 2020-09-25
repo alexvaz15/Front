3f5b11c7011511fb1ebd41d0b2eb0a77627686eb
Julian Alexander <191233@ids.upchiapas.edu.mx>
2020-09-25 00:40:59 -0500

    Add Service Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
 @NgModule({
   declarations: [
     AppComponent,
